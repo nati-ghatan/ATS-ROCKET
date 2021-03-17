d36dd2f37e8daf5991b7d5870c5e052a5dd481e1
Nati Ghatan <nati@sense.network>
2021-03-17 13:38:54 +0200

    Added randomized dilation and padding

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import numpy as np
 import torch
+import math
 import torch.nn.functional as F
 
 # Sources
@@ -10,30 +11,46 @@ import torch.nn.functional as F
 
 IN_CHANNELS = 10
 OUT_CHANNELS = 10
-RANGE_SIZE = (2, 10)
+KERNEL_SIZE_VALUES = [7, 9, 11]  # Taken from the ROCKET article
 
 
 class Net(nn.Module):
-    def __init__(self, n_kernels):
+    def __init__(self, n_kernels, data):
         super(Net, self).__init__()
         self.n_kernels = n_kernels
+        self.data = data
         self.__generate_random_kernels(self.n_kernels)
 
-    def __generate_random_kernels(self, n_kernels: int, groups=1, stride=1, ):
+    def __generate_random_kernels(self, n_kernels: int, groups=1, stride=1):
         # Initialize variables
         self.kernels = []
+        signal_length = self.data.shape[-1]
 
         # torch.nn.Conv1d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
         for kernel_index in range(n_kernels):
+            # Stride: int = 1
             # Size: int
-            current_kernel_size = np.random.randint(low=RANGE_SIZE[0], high=RANGE_SIZE[1])
-            # Stride: int
-            # Padding: int
+            current_kernel_size = np.random.choice(KERNEL_SIZE_VALUES)
+
             # Dilation: int
+            maximum_allowed_dilation = math.log2((signal_length - 1) / (current_kernel_size - 1))
+            current_dilation = np.random.randint(low=0, high=maximum_allowed_dilation)
+            dilation_factor = math.floor(math.pow(2, current_dilation))
+
+            # Padding: int
+            current_padding_size = 0 if np.random.rand() <= 0.5 else \
+                int(((current_kernel_size - 1) * dilation_factor) / 2)
+
             # Bias: bool
 
             # Create kernels with selected randomized parameters
-            self.kernels.append(nn.Conv1d(in_channels=1, out_channels=1, kernel_size=current_kernel_size))
+            self.kernels.append(nn.Conv1d(in_channels=1,
+                                          out_channels=1,
+                                          kernel_size=current_kernel_size,
+                                          stride=stride,
+                                          groups=groups,
+                                          dilation=current_dilation,
+                                          padding=current_padding_size))
 
     def forward(self, signal):
         conv_output = []
@@ -45,11 +62,16 @@ class Net(nn.Module):
 
 if __name__ == "__main__":
     # Parameters
+    surrogate_signal_length = 1000
     number_of_kernels = 3
 
     # Create surrogate data
-    surrogate_signal = torch.from_numpy(np.random.randn(10))
+    surrogate_signal = torch.from_numpy(np.random.randn(surrogate_signal_length))
+
     # Define network
-    net = Net(n_kernels=number_of_kernels)
+    net = Net(n_kernels=number_of_kernels, data=surrogate_signal)
     results = net.forward(signal=surrogate_signal)
-    h = 1
+
+    # Validate results
+    print(net.kernels)
+    print([x.shape[-1] for x in results])
