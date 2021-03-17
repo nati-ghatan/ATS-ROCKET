b6d09121b2f90523dd01229b17978471c1b621a1
Nati Ghatan <nati@sense.network>
2021-03-17 12:19:49 +0200

    Yay first commit, let's go!

---
+++ main.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+
+# Sources
+# PyTorch classifier: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+# PyTorch kernel: https://pytorch.org/docs/stable/generated/torch.nn.Conv1d.html
+# Example ROCKET git: https://github.com/angus924/rocket/blob/master/code/rocket_functions.py
+
+def generate_random_kernels(n_kernels: int) -> list(nn.Conv1d):
+    pass
+
+
+if __name__ == "__main__":
+    print("Hello world")
