31219b03c15701f9d4337efc9f7c0a242e049c84
Nati Ghatan <nati@sense.network>
2021-03-17 13:04:44 +0200

    Double vs float clash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 import numpy as np
+import torch
+import torch.nn.functional as F
 
 # Sources
 # PyTorch classifier: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
@@ -11,24 +13,40 @@ OUT_CHANNELS = 10
 RANGE_SIZE = (2, 10)
 
 
-def generate_random_kernels(n_kernels: int, groups=1, stride=1, ):
-    # Initialize variables
-    kernels = []
+class Net(nn.Module):
+    def __init__(self, n_kernels):
+        super(Net, self).__init__()
+        self.n_kernels = n_kernels
+        self.__generate_random_kernels(self.n_kernels)
 
-    # torch.nn.Conv1d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
-    for kernel_index in range(n_kernels):
-        # Size: int
-        # Stride: int
-        # Padding: int
-        # Dilation: int
-        # Bias: bool
-        kernels.append(nn.Conv1d(in_channels=1, out_channels=1, kernel_size=5))
+    def __generate_random_kernels(self, n_kernels: int, groups=1, stride=1, ):
+        # Initialize variables
+        self.kernels = []
 
-    return kernels
+        # torch.nn.Conv1d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
+        for kernel_index in range(n_kernels):
+            # Size: int
+            # Stride: int
+            # Padding: int
+            # Dilation: int
+            # Bias: bool
+            self.kernels.append(nn.Conv1d(in_channels=1, out_channels=1, kernel_size=5))
+
+    def forward(self, signal):
+        conv_output = []
+        for kernel in self.kernels:
+            reshaped_signal = signal.view(1, 1, signal.shape[-1])
+            conv_output.append(kernel(reshaped_signal))
+        return conv_output
 
 
 if __name__ == "__main__":
     # Parameters
     number_of_kernels = 3
-    random_kernels = generate_random_kernels(n_kernels=number_of_kernels)
+
+    # Create surrogate data
+    signal = torch.from_numpy(np.random.randn(10))
+    # Define network
+    net = Net(n_kernels=number_of_kernels)
+    results = net.forward(signal=signal)
     h = 1
